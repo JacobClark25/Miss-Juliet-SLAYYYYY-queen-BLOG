ed5ec7b9983d193e0f9a482c2baa73a72cddd02e
JacobClark25 <130393447+JacobClark25@users.noreply.github.com>
2024-06-15 22:47:30 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+var blog1_date = "07/15/1591 22:34";
+document.getElementById("blog-content-1d").textContent = blog1_date;
+
+var blog1 = "Sorry for the delay, my carrier pigeon has been very slow as of late, so I haven’t been able to put up as many blogs.  I just met the love of my life! <3 But there is one small problem, he is a member of the Montague family. He has brown hair and brown eyes. He is REALLY tall. I talked about him with my girlfriends, and they swoon every time I describe him. He is by far the hottest man I have ever met. He is a great kisser. And get this, he climbed over my house's guard walls to meet me tonight! He has such a distinct voice, and it’s like music to my ears. He says he wants to get married tomorrow and will tell me where to go at 9 in the morning!";
+document.getElementById("blog-content1").textContent = blog1;
+
+var blog2_date = "07/16/1591  10:27";
+document.getElementById("blog-content-2d").textContent = blog2_date;
+
+var blog2 = "My nurse met up with Romeo, and she said (after much complaining) that Romeo wants to get married at Friar Lawrence’s cell at the monastery in a few hours! I am overjoyed! I can not believe that I will be marrying the love of my life so soon! The nurse is very excited, and has told me to get ready. I hope that we can run away together and make a family! To think that in a few hours, we will be officially married, and there is nothing people can do to stop it <3  A day ago, I rejected Count Paris’ marriage proposal, but now it feels completely different. I got a new carrier pigeon as my last one broke its wing trying to deliver this to so many people. Even though it is only a few hours, it feels like an eternity waiting to get married to Romeo.";
+document.getElementById("blog-content2").textContent = blog2;
+
+var blog3_date = "07/17/1591   15:26";
+document.getElementById("blog-content-3d").textContent = blog3_date;
+
+var blog3 = "My father has arranged a marriage between me and the Count Paris. I tried to say no, but my parents say that if I don’t get married to Paris, I might as well never speak to them again. I have told Friar Lawrence, and he made me a potion to drink to fake my death so that I don’t have to marry Paris, and run away with Romeo to Mantua. The friar will be delivering a message to Romeo in the morning to prepare for when I wake up, with the location of my tomb, and the time that I will wake up. Friar Lawrence says that it will be like awaking from a refreshing sleep. He says to drink it before I go to sleep.";
+document.getElementById("blog-content3").textContent = blog3;
+
+var blog4_date = "07/17/1591    21:44";
+document.getElementById("blog-content-4d").textContent = blog4_date;
+
+var blog4 = "My father has moved the wedding up a day, as I said (to not arouse suspicion), that I would marry Paris. So I will be drinking the potion a day early. I am hoping that the letter to Romeo gets there in time! I will keep you guys posted after I wake up!";
+document.getElementById("blog-content4").textContent = blog4;
